0a4088f41d53a1ac54c69b652eedd914cfdfebc4
tonchis <lucas.tolchinsky@gmail.com>
2014-10-12 22:09:47 -0300

    Fix gpr tests.

--- git-pull-request.rb
+++ git-pull-request.rb
@@ -10,8 +10,12 @@ class GitPullRequest < Formula
   end
 
   test do
-    assert_match "https://api.github.com/repos/tonchis/homebrew-goodies/pulls", `git pull-request -f -t "hi"`
-    assert_match '{"head": "master","base": "master","title": "hi"}', `git pull-request -f -t "hi"`
+    `git init`
+    `git remote add origin git@github.com:tonchis/homebrew-goodies`
+
+    gpr_fake_run = `git-pull-request -f -t "hi"`
+    assert_match "https://api.github.com/repos/tonchis/homebrew-goodies/pulls", gpr_fake_run
+    assert_match '{"head": "master","base": "master","title": "hi"}', gpr_fake_run
   end
 end
 
