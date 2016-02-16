bb2e784751dbae9fdcd538606e65dadd4d6a9c2d
tonchis <lucas.tolchinsky@gmail.com>
2016-02-16 14:38:23 -0300

    Fix sha1 for tmuxify tar.gz

--- tmuxify.rb
+++ tmuxify.rb
@@ -3,7 +3,7 @@ require "formula"
 class Tmuxify < Formula
   homepage "https://github.com/tonchis/tmuxify"
   url "https://github.com/tonchis/tmuxify/archive/v1.2.1.tar.gz"
-  sha1 "ab4e9361e7ece0e9f55896588d596b40da268d82"
+  sha1 "b7d33be3cae2ba5f4d5c1dcca411abaa47d1a998"
 
   depends_on "tmux"
 
