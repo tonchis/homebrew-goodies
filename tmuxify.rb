0a25d19dfd0aa847df9e29ad7d1bc6cdb146c6cf
tonchis <lucas.tolchinsky@gmail.com>
2016-02-16 14:36:16 -0300

    Update tmuxify to v1.2.1

--- tmuxify.rb
+++ tmuxify.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Tmuxify < Formula
   homepage "https://github.com/tonchis/tmuxify"
-  url "https://github.com/tonchis/tmuxify/archive/v1.2.0.tar.gz"
-  sha1 "a99f2844146d69053dc3b01940c69a28a5e34468"
+  url "https://github.com/tonchis/tmuxify/archive/v1.2.1.tar.gz"
+  sha1 "ab4e9361e7ece0e9f55896588d596b40da268d82"
 
   depends_on "tmux"
 
