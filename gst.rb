0bfa67b07b091c270bf81c350201f358283fda5f
tonchis <tonchis@protonmail.com>
2017-03-21 09:58:33 -0300

    Update gst sha256.

--- gst.rb
+++ gst.rb
@@ -3,7 +3,7 @@ require "formula"
 class Gst < Formula
   homepage "https://github.com/tonchis/gst"
   url "https://github.com/tonchis/gst/archive/v1.0.0.tar.gz"
-  sha1 "d1def3c02c6cbac45fa70691cb832bc3e6537204"
+  sha256 "317c997230f976ff9acf3b3e26ff1c1d0fca39ea8bc6b92da28ff85ca1d5a8d1"
 
   def install
     bin.install "bin/gst" => "gst"
